19e6b9a7ce3560cb910e5a4b719c68c614388050
Grocery4 <juddrussel.kalalo@gmail.com>
2022-11-17 00:44:35 +0100

    💼Added Main Menu

---
+++ main.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+    int check;
+    do {
+
+        cout<<"Welcome to Scale Converter"<<endl;
+        cout<<"What would you like to do?"<<endl;
+        cout<<"1 \t Choose conversion units"<<endl;
+        cout<<"2 \t Convert"<<endl;
+        cout<<"3 \t End program"<<endl;
+        cin>>check;
+
+        switch (check)
+        {
+        case 1:
+
+            break;
+        
+        case 2:
+            break;
+
+        case 3:
+            cout<<"Ending program..."<<endl;
+            break;
+        
+        default:
+            cout<<"Invalid option! Try again";
+            break;
+        }
+
+    } while(check != 3);
+
+
+
+
+    return 0;
+}
